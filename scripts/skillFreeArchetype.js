82ab3f492b235a3c987f3dc57ab9eedd5973923e
Jonas <jonas@pop-os.localdomain>
2023-03-08 22:59:48 +0100

    [code][script] Updated manifest and added logging
    - Manifest now allows installation from github
    - Added 'Developer Mode' based log method

--- scripts/skillFreeArchetype.js
+++ scripts/skillFreeArchetype.js
@@ -1,22 +1,38 @@
 
 class SkillAsFreeArchetype {
-    static ID = 'skill-as-free-archetype';
+    static ID = 'pf2e-free-archetype-skill-feats';
 
+    /** Adds Skill feats to the list of allowed Free Archetype feats.
+     * 
+     * @param {CharacterSheetPF2e} character Object of the Character to update
+     * @param {Any} _0 
+     * @param {Any} _1 
+     * @param {Any} _2 
+     */
     static addSkillFeatType(character, _0, _1, _2) {
-        //Get character feats
         let archetype_feats = character.object.feats.get("archetype");
-        if (archetype_feats) {
-            // console.log("Adding Skill feat type to Archetype feats");
 
+        if (archetype_feats) {
+            SkillAsFreeArchetype.log(false, "Adding Skill feat type to Archetype feats");
             if (!archetype_feats.supported.includes("skill")) {
                 archetype_feats.supported.push("skill");
             }
-
         } else {
-            // console.log("Archetype feats are disabled");
+            SkillAsFreeArchetype.log(false, "Archetype feats are disabled");
+        }
+    }
+
+    static log(force, ...args) {
+        const shouldLog = force || game.modules.get('_dev-mode')?.api?.getPackageDebugValue(SkillAsFreeArchetype.ID);
+        if (shouldLog) {
+            console.log(SkillAsFreeArchetype.ID, '|', ...args);
         }
     }
 }
+
+Hooks.once('devModeReady', (DevMode) => {
+    DevMode.registerPackageDebugFlag(SkillAsFreeArchetype.ID);
+});
 
 
 Hooks.on('renderCharacterSheetPF2e', SkillAsFreeArchetype.addSkillFeatType);
